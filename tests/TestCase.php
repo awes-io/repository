93084b8b1b6a80eb27e18452481e127c81df2c94
begimov <begimov@gmail.com>
2019-05-23 09:46:41 +0300

    testcase testbench fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     /**
      * Setup the test environment.
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
